f720d22e0116c3b7d161307113a052029e9be76c
Obinna Nwabia <valinevite@gmail.com>
2020-12-02 19:08:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@ require 'pry'
 def run_guessing_game
   random_number = (rand(6) + 1)
   p "please Enter your Guess between 1 and 6:"
-  input = gets.chomp.to_i
-  if input.to_s == "exit"
+  input = gets.chomp
+  if input == "exit"
     p "Goodbye!"
-    elsif input != random_number
+    elsif input.to_i != random_number
     p "Sorry! The computer guessed #{random_number}"
   else 
     p "You guessed the correct number!"
