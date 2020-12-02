add3715040fd95defb8ec3982458ccc65107c3f3
Obinna Nwabia <valinevite@gmail.com>
2020-12-02 19:03:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 def run_guessing_game
   random_number = (rand(6) + 1)
   p "please Enter your Guess between 1 and 6:"
-  input = gets.chomp
+  input = gets.chomp.to_i
   if input == "exit"
     p "Goodbye!"
     elsif input != random_number
